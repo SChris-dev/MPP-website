20aa47c7c29863fad761fbaad3880c6529f2595c
SChris <tianyosua8@gmail.com>
2025-06-25 10:09:13 +0700

    updated sql

--- db_mpp.sql
+++ db_mpp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 22, 2025 at 04:59 AM
+-- Generation Time: Jun 25, 2025 at 03:08 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -43,6 +43,33 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `id` int NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `uploaded_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `files`
+--
+
+INSERT INTO `files` (`id`, `filename`, `path`, `uploaded_at`) VALUES
+(5, 'Kenaikan Pangkat', 'downloads/685b6405155fb_test.txt', '2025-06-25 02:26:31'),
+(12, 'Pencatuman Gelar', 'downloads/685b645f47e50_test.txt', '2025-06-25 02:52:15'),
+(13, 'Anjab/ABK', 'downloads/685b6511744f6_test.txt', '2025-06-25 02:55:13'),
+(14, 'Ijin Belajar', 'downloads/685b653eb79ee_test.txt', '2025-06-25 02:55:58'),
+(15, 'Mutasi', 'downloads/685b6560a2374_test.txt', '2025-06-25 02:56:32'),
+(16, 'Kenaikan Gaji', 'downloads/685b65819c97d_test.txt', '2025-06-25 02:57:05'),
+(17, 'Pensiun', 'downloads/685b65972b52e_test.txt', '2025-06-25 02:57:27'),
+(18, 'SOTK', 'downloads/685b65a923c55_test.txt', '2025-06-25 02:57:45');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pegawai`
 --
 
@@ -76,6 +103,12 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `username` (`username`);
 
 --
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `pegawai`
 --
 ALTER TABLE `pegawai`
@@ -90,6 +123,12 @@ ALTER TABLE `pegawai`
 --
 ALTER TABLE `admin`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `pegawai`
